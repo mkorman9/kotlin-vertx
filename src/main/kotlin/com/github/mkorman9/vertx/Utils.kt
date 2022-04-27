275736f72b5e93476ddd4a162d7164dc414662fb
Michał Korman <michal.korman@icloud.com>
2022-04-27 15:13:02 +0200

    encodePrettily -> encode

--- src/main/kotlin/com/github/mkorman9/vertx/Utils.kt
+++ src/main/kotlin/com/github/mkorman9/vertx/Utils.kt
@@ -5,5 +5,5 @@ import io.vertx.core.json.Json
 
 fun HttpServerResponse.endWithJson(obj: Any) {
     putHeader("Content-Type", "application/json")
-        .end(Json.encodePrettily(obj))
+        .end(Json.encode(obj))
 }
