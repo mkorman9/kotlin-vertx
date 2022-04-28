4f14ffca1153c1f55c39e18b89cfad70e7360279
Michał Korman <michal.korman@icloud.com>
2022-04-28 17:55:25 +0200

    log errors in error handler for status code 500

--- src/main/kotlin/com/github/mkorman9/vertx/MainRouter.kt
+++ src/main/kotlin/com/github/mkorman9/vertx/MainRouter.kt
@@ -2,11 +2,14 @@ package com.github.mkorman9.vertx
 
 import com.github.mkorman9.vertx.client.createClientRouter
 import io.vertx.core.http.HttpServerRequest
+import io.vertx.core.impl.logging.LoggerFactory
 import io.vertx.ext.web.Router
 
 class MainRouter(
     private val context: AppContext
 ) {
+    private val log = LoggerFactory.getLogger(MainRouter::class.java)
+
     private val router = Router.router(context.vertx).apply {
         mountSubRouter("/clients", createClientRouter(context))
 
@@ -25,6 +28,8 @@ class MainRouter(
         }
 
         errorHandler(500) { ctx ->
+            log.error("Unexpected error while serving request", ctx.failure())
+
             ctx.response().endWithJson(StatusDTO(
                 status = "error",
                 message = "internal server error"
