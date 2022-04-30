e950b35d64fe4b5f131c3c9e6f9e054f3e45bcd9
Michał Korman <michal.korman@icloud.com>
2022-04-30 22:52:40 +0200

    force UTC timezone for local run

--- src/main/kotlin/com/github/mkorman9/vertx/Main.kt
+++ src/main/kotlin/com/github/mkorman9/vertx/Main.kt
@@ -4,6 +4,7 @@ import io.vertx.core.Vertx
 
 fun main() {
     System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory")
+    System.setProperty("user.timezone", "UTC")
 
     val vertx = Vertx.vertx()
     vertx.deployVerticle(MainVerticle())
