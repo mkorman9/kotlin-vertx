e477204c7d90c501b00888a5ed88053f90f6b2e1
Michał Korman <michal.korman@icloud.com>
2022-05-02 22:33:33 +0200

    simplify ShutdownHook

--- src/main/kotlin/com/github/mkorman9/vertx/ShutdownHook.kt
+++ src/main/kotlin/com/github/mkorman9/vertx/ShutdownHook.kt
@@ -4,22 +4,13 @@ import io.vertx.core.Vertx
 
 class ShutdownHook {
     companion object {
-        private const val CHECK_INTERVAL_MS: Long = 100
-
         fun register(vertx: Vertx) {
             Runtime.getRuntime().addShutdownHook(object : Thread() {
                 override fun run() {
-                    val closing = vertx.close()
+                    vertx.close()
                         .toCompletionStage()
                         .toCompletableFuture()
-
-                    while (true) {
-                        if (closing.isDone || closing.isCancelled) {
-                            break
-                        }
-
-                        sleep(CHECK_INTERVAL_MS)
-                    }
+                        .join()
                 }
             })
         }
