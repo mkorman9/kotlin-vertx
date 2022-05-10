2258f6f3844208646a141499e42bda97fd6d5f05
Michał Korman <michal.korman@icloud.com>
2022-05-10 23:39:44 +0200

    use SHA1PRNG algorithm on Windows

--- src/main/kotlin/com/github/mkorman9/vertx/utils/SecureRandomGenerator.kt
+++ src/main/kotlin/com/github/mkorman9/vertx/utils/SecureRandomGenerator.kt
@@ -4,15 +4,23 @@ import java.security.SecureRandom
 
 class SecureRandomGenerator {
     companion object {
-        private val random = SecureRandom.getInstance("NativePRNGNonBlocking")
-        private const val charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        private val random = SecureRandom.getInstance(getPRNGAlgorithm())
 
+        private const val charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         fun generate(length: Long): String {
             return random
                 .ints(length, 0, charset.length)
                 .mapToObj { charset[it] }
                 .collect({ java.lang.StringBuilder() }, StringBuilder::append, StringBuilder::append)
                 .toString()
+        }
+
+        private fun getPRNGAlgorithm(): String {
+            return if (System.getProperty("os.name").contains("win")) {
+                "SHA1PRNG"
+            } else {
+                "NativePRNGNonBlocking"
+            }
         }
     }
 }
