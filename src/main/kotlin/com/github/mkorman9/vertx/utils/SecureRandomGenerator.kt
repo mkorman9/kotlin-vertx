4768cd4203b6adb54cc3b05edd60b4c4c5a74f69
Michał Korman <michal.korman@icloud.com>
2022-05-10 23:41:05 +0200

    formatting

--- src/main/kotlin/com/github/mkorman9/vertx/utils/SecureRandomGenerator.kt
+++ src/main/kotlin/com/github/mkorman9/vertx/utils/SecureRandomGenerator.kt
@@ -5,8 +5,8 @@ import java.security.SecureRandom
 class SecureRandomGenerator {
     companion object {
         private val random = SecureRandom.getInstance(getPRNGAlgorithm())
-
         private const val charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
         fun generate(length: Long): String {
             return random
                 .ints(length, 0, charset.length)
