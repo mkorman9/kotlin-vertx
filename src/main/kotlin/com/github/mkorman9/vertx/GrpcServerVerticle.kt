276ceb434f453ddd2c7531e8768c67dbde351ba9
Michał Korman <michal.korman@icloud.com>
2022-06-02 15:47:48 +0200

    add protobuf and gRPC

---
+++ src/main/kotlin/com/github/mkorman9/vertx/GrpcServerVerticle.kt
@@ -0,0 +1,55 @@
+package com.github.mkorman9.vertx
+
+import com.github.mkorman9.vertx.client.ClientServiceGrpcImpl
+import com.github.mkorman9.vertx.utils.DeployVerticle
+import com.google.inject.Injector
+import dev.misfitlabs.kotlinguice4.getInstance
+import io.grpc.Server
+import io.grpc.ServerBuilder
+import io.vertx.config.ConfigRetriever
+import io.vertx.core.impl.logging.LoggerFactory
+import io.vertx.kotlin.coroutines.CoroutineVerticle
+import io.vertx.kotlin.coroutines.await
+
+@DeployVerticle
+class GrpcServerVerticle(
+    passedInjector: Injector? = null
+) : CoroutineVerticle() {
+    private val log = LoggerFactory.getLogger(GrpcServerVerticle::class.java)
+
+    private val injector: Injector = passedInjector ?: BootstrapVerticle.injector
+    private lateinit var server: Server
+
+    override suspend fun start() {
+        try {
+            val configRetriever = injector.getInstance<ConfigRetriever>()
+            val config = configRetriever.config.await()
+
+            val port = config.getJsonObject("grpc")?.getInteger("port") ?: 9090
+
+            val clientsService = injector.getInstance<ClientServiceGrpcImpl>()
+
+            vertx.executeBlocking<Void> { call ->
+                server = ServerBuilder
+                    .forPort(port)
+                    .addService(clientsService)
+                    .build()
+                    .start()
+                call.complete()
+            }.await()
+
+            log.info("GrpcServerVerticle has been deployed")
+        } catch (e: Exception) {
+            log.error("Failed to deploy GrpcServerVerticle", e)
+        }
+    }
+
+    override suspend fun stop() {
+        vertx.executeBlocking<Void> { call ->
+            server.shutdown().awaitTermination()
+            call.complete()
+        }.await()
+
+        log.info("GrpcServerVerticle has been stopped")
+    }
+}
---
+++ src/main/kotlin/com/github/mkorman9/vertx/client/ClientServiceGrpcImpl.kt
@@ -0,0 +1,62 @@
+package com.github.mkorman9.vertx.client
+
+import com.github.mkorman9.vertx.Client
+import com.github.mkorman9.vertx.ClientRequest
+import com.github.mkorman9.vertx.ClientServiceGrpcKt
+import com.google.protobuf.Timestamp
+import io.vertx.kotlin.coroutines.await
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ClientServiceGrpcImpl @Inject constructor(
+    private val clientRepository: ClientRepository
+) : ClientServiceGrpcKt.ClientServiceCoroutineImplBase() {
+    override fun getClients(request: ClientRequest): Flow<Client> {
+        return flow {
+            val clientsPage = clientRepository.findPaged(
+                ClientsFilteringOptions(),
+                ClientsPagingOptions(1, 10),
+                ClientsSortingOptions("id", false)
+            ).await()
+
+            clientsPage.data
+                .map {
+                    Client.newBuilder()
+                        .setId(it.id.toString())
+                        .setGender(it.gender)
+                        .setFirstName(it.firstName)
+                        .setLastName(it.lastName)
+                        .setAddress(it.address)
+                        .setPhoneNumber(it.phoneNumber)
+                        .setEmail(it.email)
+                        .setBirthDate(toTimestamp(it.birthDate))
+                        .addAllCreditCards(it.creditCards.map {
+                            com.github.mkorman9.vertx.CreditCard.newBuilder()
+                                .setNumber(it.number)
+                                .build()
+                        })
+                        .build()
+                }
+                .forEach {
+                    emit(it)
+                }
+        }
+    }
+
+    private fun toTimestamp(dateTime: LocalDateTime?): Timestamp? {
+        if (dateTime == null) {
+            return null
+        }
+
+        val instant = dateTime.toInstant(ZoneOffset.UTC)
+        return Timestamp.newBuilder()
+            .setSeconds(instant.epochSecond)
+            .setNanos(instant.nano)
+            .build()
+    }
+}
