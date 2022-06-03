ed4295d86bd9e04c6941393c2e24b184f330d151
Michał Korman <michal.korman@icloud.com>
2022-06-03 14:23:50 +0200

    delete DB schema

--- deployment/kubernetes-env/up.sh
+++ deployment/kubernetes-env/up.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-SCHEMA_DIR="$(realpath ${SCRIPTPATH}/../../schema)"
-TESTDATA_DIR="$(realpath ${SCRIPTPATH}/../../testdata)"
 
 $SCRIPTPATH/down.sh &> /dev/null
 
