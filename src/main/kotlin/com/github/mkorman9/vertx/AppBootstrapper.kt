bfdecf0f0f107b324f8b38b153e0035171ae523b
Michał Korman <michal.korman@icloud.com>
2022-06-11 21:06:52 +0200

    make app configurable via environment variables

--- src/main/kotlin/com/github/mkorman9/vertx/AppBootstrapper.kt
+++ src/main/kotlin/com/github/mkorman9/vertx/AppBootstrapper.kt
@@ -94,25 +94,59 @@ class AppBootstrapper {
     }
 
     private fun createConfigRetriever(vertx: Vertx): ConfigRetriever {
-        val store = ConfigStoreOptions()
+        val configFileStore = ConfigStoreOptions()
             .setType("file")
             .setFormat("yaml")
             .setOptional(true)
             .setConfig(JsonObject()
                 .put("path", System.getenv().getOrDefault("CONFIG_FILE", "./config.yml"))
             )
-        val secretsStore = ConfigStoreOptions()
+        val secretsFileStore = ConfigStoreOptions()
             .setType("file")
             .setFormat("yaml")
             .setOptional(true)
             .setConfig(JsonObject()
                 .put("path", System.getenv().getOrDefault("SECRETS_FILE", "./secrets.yml"))
             )
+        val envVarsStore = ConfigStoreOptions()
+            .setType("env")
 
         return ConfigRetriever.create(vertx, ConfigRetrieverOptions()
-            .addStore(store)
-            .addStore(secretsStore)
+            .addStore(configFileStore)
+            .addStore(secretsFileStore)
+            .addStore(envVarsStore)
         )
+            .setConfigurationProcessor { config ->
+                val newConfig = mutableMapOf<String, Any>()
+
+                config.map.forEach { entry ->
+                    val key = entry.key.lowercase()
+                    val value = entry.value
+
+                    val splits = key.split("_")
+                    if (splits.size == 1) {
+                        newConfig[key] = value
+                    } else {
+                        var ptr = newConfig
+                        splits.forEachIndexed { index, s ->
+                            if (index < splits.size - 1) {
+                                if (!ptr.containsKey(s)) {
+                                    ptr[s] = mutableMapOf<String, Any>()
+                                }
+
+                                if (ptr[s] is Map<*, *>) {
+                                    @Suppress("UNCHECKED_CAST")
+                                    ptr = ptr[s] as MutableMap<String, Any>
+                                }
+                            } else {
+                                ptr[s] = value
+                            }
+                        }
+                    }
+                }
+
+                JsonObject(newConfig)
+            }
     }
 
     private fun readVersionFromManifest(): String {
