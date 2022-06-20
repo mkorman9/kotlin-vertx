096dc84ea18d3f1cfad69b54bc360b1a0fa18349
Michał Korman <michal.korman@icloud.com>
2022-06-20 19:38:05 +0200

    add more options for configuring redis pool

--- src/main/kotlin/com/github/mkorman9/vertx/tools/redis/RedisInitializer.kt
+++ src/main/kotlin/com/github/mkorman9/vertx/tools/redis/RedisInitializer.kt
@@ -13,9 +13,17 @@ class RedisInitializer {
         fun initialize(vertx: Vertx, config: Config): Future<RedisAPI> {
             val uri = config.get<String>("redis.uri") ?: throw RuntimeException("redis.uri is missing from config")
             val password = config.get<String>("redis.password")
+            val poolSize = config.get<Int>("redis.pool.size") ?: 6
+            val poolWaiting = config.get<Int>("redis.pool.waiting") ?: 24
+            val poolCleanerInterval = config.get<Int>("redis.pool.cleaner.interval") ?: 30_000
+            val poolRecycleTimeout = config.get<Int>("redis.pool.recycle.timeout") ?: 180_000
 
             val options = RedisOptions()
             options.setConnectionString(uri)
+            options.maxPoolSize = poolSize
+            options.maxPoolWaiting = poolWaiting
+            options.poolCleanerInterval = poolCleanerInterval
+            options.poolRecycleTimeout = poolRecycleTimeout
 
             if (password != null) {
                 options.password = password
