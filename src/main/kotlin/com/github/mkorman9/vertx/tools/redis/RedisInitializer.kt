ef4c65a3e592991ee6591a68f09ef9afd30672d3
Michał Korman <michal.korman@icloud.com>
2022-06-28 15:20:44 +0200

    utilise object syntax

--- src/main/kotlin/com/github/mkorman9/vertx/tools/redis/RedisInitializer.kt
+++ src/main/kotlin/com/github/mkorman9/vertx/tools/redis/RedisInitializer.kt
@@ -8,30 +8,28 @@ import io.vertx.redis.client.Redis
 import io.vertx.redis.client.RedisAPI
 import io.vertx.redis.client.RedisOptions
 
-class RedisInitializer {
-    companion object {
-        fun initialize(vertx: Vertx, config: Config): Future<RedisAPI> {
-            val uri = config.get<String>("redis.uri") ?: throw RuntimeException("redis.uri is missing from config")
-            val password = config.get<String>("redis.password")
-            val poolSize = config.get<Int>("redis.pool.size") ?: 6
-            val poolWaiting = config.get<Int>("redis.pool.waiting") ?: 24
-            val poolCleanerInterval = config.get<Int>("redis.pool.cleaner.interval") ?: 30_000
-            val poolRecycleTimeout = config.get<Int>("redis.pool.recycle.timeout") ?: 180_000
+object RedisInitializer {
+    fun initialize(vertx: Vertx, config: Config): Future<RedisAPI> {
+        val uri = config.get<String>("redis.uri") ?: throw RuntimeException("redis.uri is missing from config")
+        val password = config.get<String>("redis.password")
+        val poolSize = config.get<Int>("redis.pool.size") ?: 6
+        val poolWaiting = config.get<Int>("redis.pool.waiting") ?: 24
+        val poolCleanerInterval = config.get<Int>("redis.pool.cleaner.interval") ?: 30_000
+        val poolRecycleTimeout = config.get<Int>("redis.pool.recycle.timeout") ?: 180_000
 
-            val options = RedisOptions()
-            options.setConnectionString(uri)
-            options.maxPoolSize = poolSize
-            options.maxPoolWaiting = poolWaiting
-            options.poolCleanerInterval = poolCleanerInterval
-            options.poolRecycleTimeout = poolRecycleTimeout
+        val options = RedisOptions()
+        options.setConnectionString(uri)
+        options.maxPoolSize = poolSize
+        options.maxPoolWaiting = poolWaiting
+        options.poolCleanerInterval = poolCleanerInterval
+        options.poolRecycleTimeout = poolRecycleTimeout
 
-            if (password != null) {
-                options.password = password
-            }
-
-            return Redis.createClient(vertx, options)
-                .connect()
-                .map { connection -> RedisAPI.api(connection) }
+        if (password != null) {
+            options.password = password
         }
+
+        return Redis.createClient(vertx, options)
+            .connect()
+            .map { connection -> RedisAPI.api(connection) }
     }
 }
