c2b1a77837f34778b0f7ac34863a1b97ac2c9634
Michał Korman <michal.korman@icloud.com>
2022-06-28 23:42:01 +0200

    make VerticleDeployer verify if class is in fact a Verticle

--- src/main/kotlin/com/github/mkorman9/vertx/utils/VerticleDeployer.kt
+++ src/main/kotlin/com/github/mkorman9/vertx/utils/VerticleDeployer.kt
@@ -13,34 +13,52 @@ object VerticleDeployer {
 
         ReflectionsUtils.findClasses(packageName, DeployVerticle::class.java)
             .forEach { c ->
-                val annotation = c.annotations.filterIsInstance<DeployVerticle>()
-                    .first()
-                val instances = calculateInstancesNumber(annotation.scalingStrategy, annotation.minInstances)
-
-                for (i in 0 until instances) {
-                    val instance = c.declaredConstructors[0].newInstance() as Verticle
-                    if (instance is ContextualVerticle) {
-                        instance.injector = injector
-                    }
-
-                    val future = vertx.deployVerticle(
-                        instance,
-                        DeploymentOptions()
-                            .setConfig(config)
-                            .setWorker(annotation.worker)
-                            .setWorkerPoolName(annotation.workerPoolName.ifEmpty { null })
-                            .setWorkerPoolSize(annotation.workerPoolSize)
-                    )
-                    futures.add(future)
+                if (Verticle::class.java.isAssignableFrom(c)) {
+                    val f = deployVerticlesFromClass(c, vertx, config, injector)
+                    futures.addAll(f)
+                } else {
+                    log.warn("Class ${c.name} annotated with @DeployVerticle does not implement Verticle interface")
                 }
-
-                log.info("Deployed $instances instances of ${c.simpleName}")
             }
 
         CompositeFuture.all(futures)
             .toCompletionStage()
             .toCompletableFuture()
             .join()
+    }
+
+    private fun deployVerticlesFromClass(
+        c: Class<Any>,
+        vertx: Vertx,
+        config: Config,
+        injector: Injector
+    ): List<Future<*>> {
+        val futures = mutableListOf<Future<*>>()
+
+        val annotation = c.annotations.filterIsInstance<DeployVerticle>()
+            .first()
+        val instances = calculateInstancesNumber(annotation.scalingStrategy, annotation.minInstances)
+
+        for (i in 0 until instances) {
+            val instance = c.declaredConstructors[0].newInstance() as Verticle
+            if (instance is ContextualVerticle) {
+                instance.injector = injector
+            }
+
+            val future = vertx.deployVerticle(
+                instance,
+                DeploymentOptions()
+                    .setConfig(config)
+                    .setWorker(annotation.worker)
+                    .setWorkerPoolName(annotation.workerPoolName.ifEmpty { null })
+                    .setWorkerPoolSize(annotation.workerPoolSize)
+            )
+            futures.add(future)
+        }
+
+        log.info("Deployed $instances instances of ${c.simpleName}")
+
+        return futures
     }
 
     private fun calculateInstancesNumber(scalingStrategy: VerticesScalingStrategy, minInstances: Int): Int {
