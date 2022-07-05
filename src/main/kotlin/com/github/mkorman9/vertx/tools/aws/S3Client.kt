27e5d3acef61d36b3e8a9171c9586930b32e2565
Michał Korman <michal.korman@icloud.com>
2022-07-05 22:59:36 +0200

    implement S3Client

---
+++ src/main/kotlin/com/github/mkorman9/vertx/tools/aws/S3Client.kt
@@ -0,0 +1,65 @@
+package com.github.mkorman9.vertx.tools.aws
+
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectRequest
+import io.vertx.core.Future
+import io.vertx.core.Vertx
+import io.vertx.core.buffer.Buffer
+import java.io.ByteArrayInputStream
+
+class S3Client private constructor() {
+    companion object {
+        fun create(): S3Client {
+            return S3Client()
+        }
+
+        private const val READ_BUFFER_SIZE = 1024
+    }
+
+    private val client: AmazonS3 = AmazonS3ClientBuilder
+        .standard()
+        .build()
+
+    fun getObject(vertx: Vertx, bucketName: String, key: String): Future<Buffer> {
+        return vertx.executeBlocking { call ->
+            try {
+                val obj = client.getObject(bucketName, key)
+                val contentStream = obj.objectContent
+                val output = Buffer.buffer()
+
+                val buffer = ByteArray(READ_BUFFER_SIZE)
+                while (true) {
+                    val bytesRead = contentStream.read(buffer)
+                    if (bytesRead == -1) {
+                        break
+                    }
+
+                    output.appendBytes(buffer)
+                }
+
+                call.complete(output)
+            } catch (e: Exception) {
+                call.fail(e)
+            }
+        }
+    }
+
+    fun putObject(vertx: Vertx, bucketName: String, key: String, content: Buffer): Future<Void> {
+        return vertx.executeBlocking { call ->
+            try {
+                client.putObject(
+                    bucketName,
+                    key,
+                    ByteArrayInputStream(content.bytes),
+                    ObjectMetadata()
+                )
+
+                call.complete()
+            } catch (e: Exception) {
+                call.fail(e)
+            }
+        }
+    }
+}
