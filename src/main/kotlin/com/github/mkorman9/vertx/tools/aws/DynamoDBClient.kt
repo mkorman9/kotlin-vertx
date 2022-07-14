4324c3571398b390944d2ddf0b497092d6f4c9b5
Michał Korman <michal.korman@icloud.com>
2022-07-14 17:46:25 +0200

    add DynamoDB client

---
+++ src/main/kotlin/com/github/mkorman9/vertx/tools/aws/DynamoDBClient.kt
@@ -0,0 +1,153 @@
+package com.github.mkorman9.vertx.tools.aws
+
+import com.amazonaws.AmazonWebServiceRequest
+import com.amazonaws.ClientConfiguration
+import com.amazonaws.Protocol
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.client.builder.AwsClientBuilder
+import com.amazonaws.handlers.AsyncHandler
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper
+import com.amazonaws.services.dynamodbv2.model.*
+import com.amazonaws.waiters.WaiterHandler
+import com.amazonaws.waiters.WaiterParameters
+import com.github.mkorman9.vertx.utils.Config
+import com.github.mkorman9.vertx.utils.get
+import io.vertx.core.Future
+import io.vertx.core.Promise
+
+class DynamoDBClient private constructor(
+    config: Config
+) {
+    companion object {
+        fun create(config: Config): DynamoDBClient {
+            return DynamoDBClient(config)
+        }
+    }
+
+    val client: AmazonDynamoDBAsync
+    val mapper: DynamoDBMapper
+
+    init {
+        val emulatorEnabled = config.get<Boolean>("aws.dynamodb.emulator.enabled") ?: false
+        val emulatorAddress = config.get<String>("aws.dynamodb.emulator.address") ?: "localhost:4100"
+
+        if (emulatorEnabled) {
+            client = AmazonDynamoDBAsyncClientBuilder.standard()
+                .withEndpointConfiguration(
+                    AwsClientBuilder.EndpointConfiguration(
+                        emulatorAddress,
+                        Regions.US_EAST_1.name
+                    )
+                )
+                .withCredentials(
+                    AWSStaticCredentialsProvider(
+                        BasicAWSCredentials("ACCESS_KEY", "SECRET_ACCESS_KEY")
+                    )
+                )
+                .withClientConfiguration(
+                    ClientConfiguration()
+                        .withProtocol(Protocol.HTTP)
+                )
+                .build()
+        } else {
+            client = AmazonDynamoDBAsyncClientBuilder.defaultClient()
+        }
+
+        mapper = DynamoDBMapper(client)
+    }
+
+    fun close() {
+        client.shutdown()
+    }
+
+    inline fun <reified T> createTable(
+        billingMode: BillingMode = BillingMode.PAY_PER_REQUEST
+    ): Future<Void> {
+        val request = mapper.generateCreateTableRequest(T::class.java)
+
+        val createTablePromise = Promise.promise<CreateTableResult>()
+
+        client.createTableAsync(
+            request
+                .withBillingMode(billingMode),
+            createAsyncHandler<CreateTableRequest, CreateTableResult>(createTablePromise)
+        )
+
+        val resultPromise = Promise.promise<Void>()
+
+        createTablePromise.future()
+            .recover { cause ->
+                if (cause.message?.startsWith("Table already exists:") == true) {
+                    Future.succeededFuture(null)
+                } else {
+                    Future.failedFuture(cause)
+                }
+            }
+            .onSuccess {
+                waitForTableActive(request.tableName)
+                    .onSuccess { resultPromise.complete() }
+                    .onFailure { cause -> resultPromise.fail(cause) }
+            }
+            .onFailure { cause ->
+                resultPromise.fail(cause)
+            }
+
+        return resultPromise.future()
+    }
+
+    inline fun <reified T, H, R> putItem(item: T): Future<PutItemResult> {
+        val tableName = mapper.generateCreateTableRequest(T::class.java).tableName
+        val tableModel = mapper.getTableModel(T::class.java)
+        val attributes = tableModel.convertKey<H, R>(item)
+
+        val promise = Promise.promise<PutItemResult>()
+
+        client.putItemAsync(
+            PutItemRequest()
+                .withTableName(tableName)
+                .withItem(attributes),
+            createAsyncHandler<PutItemRequest, PutItemResult>(promise)
+        )
+
+        return promise.future()
+    }
+
+    fun <REQUEST : AmazonWebServiceRequest, RESULT> createAsyncHandler(
+        promise: Promise<RESULT>
+    ): AsyncHandler<REQUEST, RESULT> {
+        return object : AsyncHandler<REQUEST, RESULT> {
+            override fun onError(exception: Exception?) {
+                promise.fail(exception)
+            }
+
+            override fun onSuccess(request: REQUEST, result: RESULT) {
+                promise.complete(result)
+            }
+        }
+    }
+
+    fun waitForTableActive(tableName: String): Future<Void> {
+        val promise = Promise.promise<Void>()
+
+        client.waiters().tableExists().runAsync(
+            WaiterParameters(
+                DescribeTableRequest()
+                    .withTableName(tableName)
+            ),
+            object : WaiterHandler<AmazonWebServiceRequest>() {
+                override fun onWaitSuccess(request: AmazonWebServiceRequest?) {
+                    promise.complete()
+                }
+
+                override fun onWaitFailure(e: java.lang.Exception?) {
+                    promise.fail(e)
+                }
+            })
+
+        return promise.future()
+    }
+}
