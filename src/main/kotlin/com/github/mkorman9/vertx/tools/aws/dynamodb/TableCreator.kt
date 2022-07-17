bc3c3310b8b4465a33744fa53cdb7df5c9dcb744
Michał Korman <michal.korman@icloud.com>
2022-07-17 23:33:25 +0200

    add TableCreator to dynamodb module

---
+++ src/main/kotlin/com/github/mkorman9/vertx/tools/aws/dynamodb/TableCreator.kt
@@ -0,0 +1,31 @@
+package com.github.mkorman9.vertx.tools.aws.dynamodb
+
+import com.amazonaws.services.dynamodbv2.model.BillingMode
+import io.vertx.core.CompositeFuture
+import io.vertx.core.Future
+
+object TableCreator {
+    fun createAll(client: DynamoDBClient, definitions: List<TableDefinition>): CompositeFuture {
+        val futures = mutableListOf<Future<*>>()
+
+        definitions.forEach { tableDefinition ->
+            val f = client.createTable(
+                tableClass = tableDefinition.tableClass,
+                billingMode = tableDefinition.billingMode,
+                readCapacity = tableDefinition.readCapacity,
+                writeCapacity = tableDefinition.writeCapacity
+            )
+
+            futures.add(f)
+        }
+
+        return CompositeFuture.all(futures)
+    }
+}
+
+data class TableDefinition(
+    val tableClass: Class<*>,
+    val billingMode: BillingMode = BillingMode.PAY_PER_REQUEST,
+    val readCapacity: Long = 6000,
+    val writeCapacity: Long = 2000,
+)
