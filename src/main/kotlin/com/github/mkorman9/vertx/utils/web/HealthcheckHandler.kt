5767ffaa914bc425b7746fd89dad1e88df06b7e7
Michał Korman <michal.korman@icloud.com>
2022-08-06 18:23:53 +0200

    make handlers objects

--- src/main/kotlin/com/github/mkorman9/vertx/utils/web/HealthcheckHandler.kt
+++ src/main/kotlin/com/github/mkorman9/vertx/utils/web/HealthcheckHandler.kt
@@ -4,20 +4,18 @@ import com.github.mkorman9.vertx.utils.DeploymentInfo
 import io.vertx.core.Handler
 import io.vertx.ext.web.RoutingContext
 
-class HealthcheckHandler {
-    companion object {
-        private val info = DeploymentInfo.get()
+object HealthcheckHandler {
+    private val info = DeploymentInfo.get()
 
-        fun create(): Handler<RoutingContext> = Handler { ctx ->
-            ctx.response().endWithJson(
-                HealthcheckResponse(
-                    status = "healthy",
-                    version = info.version,
-                    startupTime = info.startupTime,
-                    environment = info.environment,
-                    profile = info.profile
-                )
+    fun create(): Handler<RoutingContext> = Handler { ctx ->
+        ctx.response().endWithJson(
+            HealthcheckResponse(
+                status = "healthy",
+                version = info.version,
+                startupTime = info.startupTime,
+                environment = info.environment,
+                profile = info.profile
             )
-        }
+        )
     }
 }
--- src/main/kotlin/com/github/mkorman9/vertx/utils/web/SecurityHeadersHandler.kt
+++ src/main/kotlin/com/github/mkorman9/vertx/utils/web/SecurityHeadersHandler.kt
@@ -5,27 +5,25 @@ import io.vertx.core.Handler
 import io.vertx.ext.web.RoutingContext
 import com.github.mkorman9.vertx.utils.get
 
-class SecurityHeadersHandler {
-    companion object {
-        fun create(config: Config): Handler<RoutingContext> {
-            val isEnabled = config.get<Boolean>("server.security.headers") ?: false
+object SecurityHeadersHandler {
+    fun create(config: Config): Handler<RoutingContext> {
+        val isEnabled = config.get<Boolean>("server.security.headers") ?: false
 
-            return Handler { ctx ->
-                if (isEnabled) {
-                    ctx.response().putHeader("X-Frame-Options", "DENY")
-                    ctx.response().putHeader("X-Content-Type-Options", "nosniff")
-                    ctx.response().putHeader("X-XSS-Protection", "0")
+        return Handler { ctx ->
+            if (isEnabled) {
+                ctx.response().putHeader("X-Frame-Options", "DENY")
+                ctx.response().putHeader("X-Content-Type-Options", "nosniff")
+                ctx.response().putHeader("X-XSS-Protection", "0")
 
-                    if (ctx.request().isClientTLS()) {
-                        ctx.response().putHeader(
-                            "Strict-Transport-Security",
-                            "max-age=63072000; includeSubDomains; preload"
-                        )
-                    }
+                if (ctx.request().isClientTLS()) {
+                    ctx.response().putHeader(
+                        "Strict-Transport-Security",
+                        "max-age=63072000; includeSubDomains; preload"
+                    )
                 }
-
-                ctx.next()
             }
+
+            ctx.next()
         }
     }
 }
