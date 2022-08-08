20ea7b7a8f36e6befa29317cf528330273c5c6ee
Michał Korman <michal.korman@icloud.com>
2022-08-08 18:56:32 +0200

    refactor Kubernetes-related config

--- deployment/kubernetes-env/down.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-deployment_name="${DEPLOYMENT_NAME:-kotlin-vertx-local-environment}"
-namespace="${KUBE_NAMESPACE:-kotlin-vertx}"
-
-echo "Shutting down the environment"
-helm uninstall "$deployment_name" --namespace="$namespace" || true
